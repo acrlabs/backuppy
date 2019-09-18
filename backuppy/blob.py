fa42458a677ffe3e3bc20aca11753849c060fc10
David Morrison <drmorr0@gmail.com>
2019-09-17 21:41:20 -0700

    switch to bsdiff4 for diff computations

--- backuppy/blob.py
+++ backuppy/blob.py
@@ -1,10 +1,10 @@
 import os
-import re
 from itertools import zip_longest
 from typing import Optional
 from typing import Tuple
 
-import edlib
+import bsdiff4
+import colorlog
 
 from backuppy.exceptions import DiffParseError
 from backuppy.exceptions import DiffTooLargeException
@@ -12,26 +12,15 @@ from backuppy.io import IOIter
 
 # This file will read and write diff file for backuppy.
 
-# The expected diff format is '(@<pos>|<action><contents-length>|<optional-contents>)*'
+# The expected diff format is '(<len>\|<byte-stream>)*'
 #
 # The meaning of these tokens is:
-#  - pos: the location in the original file to which the diff should be applied (given any previous
-#      diffs that have already been applied).
-#  - action: one of 'D' (delete), 'I' (insert), 'X' (replace)
-#  - contents-length: how many bytes of data to delete, insert, or replace
-#  - optional-contents: if the action was insert or replace, the data to insert or replace; this
-#      field is omitted (but the preceding separator | is not) for delete actions
-#
-# For example, if the original file is 'asdf' and the new file is 'adz foobar' the diff would be
-# be '@1|D1|@2|I2|z @5|I5|oobar'.  Note that the position of the second chunk (2) is _after_ the
-# first diff chunk has been applied, and similarly for the third diff chunk.
+#  - len: the number of bytes in the diff
+#  - byte-stream: a bytes diff returned by bsdiff4
 
 
-class Token:
-    DEL = b'D'
-    INS = b'I'
-    REPL = b'X'
-    SEP = b'|'
+logger = colorlog.getLogger(__name__)
+SEPARATOR = b'|'
 
 
 def apply_diff(orig_file: IOIter, diff_file: IOIter, new_file: IOIter) -> None:
@@ -44,51 +33,32 @@ def apply_diff(orig_file: IOIter, diff_file: IOIter, new_file: IOIter) -> None:
 
     # The outer loop reads a chunk of data at a time; the inner loop parses
     # the read chunk one step at a time and applies it
-    diff, offset = b'', 0
-    writer = new_file.writer(); next(writer)
+    diff = b''
+    new_writer = new_file.writer(); next(new_writer)
+    orig_reader = orig_file.reader()
+    logger.debug2('applying diff')
     for diff_chunk in diff_file.reader():
         diff += diff_chunk
         while diff:
             # try to parse the next chunk; if we can't, break out of the loop to get more data
             try:
-                position, action_len, remainder = diff.split(Token.SEP, 2)
+                diff_len_str, remainder = diff.split(SEPARATOR, 1)
             except ValueError:
                 break
 
-            # Use slices to get bytes objects instead of integers
-            contents_pos = int(position[1:])  # skip the initial '@'
-            action = action_len[0:1]
-            contents_len = int(action_len[1:])
-
-            # scan in the original file until we get to the start position for this diff
-            for data in orig_file.reader(end=contents_pos - offset, reset_pos=False):
-                writer.send(data)
-
-            # If the remainder of the chunk doesn't have enough bytes and we need to insert
-            # or replace data, get the next chunk first so we have all the needed data
-            if len(remainder) < contents_len and action != Token.DEL:
+            diff_len = int(diff_len_str)
+            if len(remainder) < diff_len:
                 break
-            contents = b'' if action == Token.DEL else remainder[:contents_len]
-            diff = remainder if action == Token.DEL else remainder[contents_len:]
-            if action == Token.DEL:
-                orig_file.fd.seek(contents_len, 1)  # seek from the current position
-                offset -= contents_len  # adjust the orig_file position based on the amount deleted
-            elif action == Token.INS:
-                writer.send(contents)
-                offset += contents_len  # adjust the orig_file position based on the amount inserted
-            elif action == Token.REPL:
-                writer.send(contents)
-                orig_file.fd.seek(contents_len, 1)  # seek from the current position
-            else:
-                raise DiffParseError(f'Expected an action, found {action}')
+
+            try:
+                orig_block = next(orig_reader)
+            except StopIteration:
+                orig_block = b''
+            new_writer.send(bsdiff4.patch(orig_block, remainder[:diff_len]))
+            diff = remainder[diff_len:]
 
     if diff:
         raise DiffParseError(f'Un-parseable diff: {diff}')
-
-    # If we get here and there's still data in the original file, it must be equal to what was in
-    # the new file, so just copy any remaining data from the original file to the new file
-    for data in orig_file.reader(reset_pos=False):
-        writer.send(data)
 
 
 def compute_sha_and_diff(
@@ -104,38 +74,17 @@ def compute_sha_and_diff(
     :param diff_file: an IOIter object where the diff data will be written
     """
 
-    pos, diff_size = 0, 0
-    writer = diff_file.writer(); next(writer)
+    total_written = 0
     orig_size = os.fstat(orig_file.fd.fileno()).st_size
-    for orig_bytes, new_bytes in zip_longest(orig_file.reader(), new_file.reader()):
-        if not orig_bytes:
-            steps = [(len(new_bytes), Token.INS)]
-        elif not new_bytes:
-            steps = [(len(orig_bytes), Token.DEL)]
-        else:
-            # Reverse the order of new_contents and old_contents since edlib outputs the cigar
-            # w.r.t. the 2nd arg
-            result = edlib.align(new_bytes, orig_bytes, task='path')
-            steps = [(int(n), c.encode('utf-8'))
-                     for n, c in re.findall(r'(\d+)([=DIX])', result['cigar'])]
 
-        local_pos, diff = 0, b''
-        for num, action in steps:
-            local_pos += num
-            if action == b'=':
-                continue
-
-            diff += f'@{pos+local_pos-num}'.encode('utf-8') + Token.SEP + action
-            if action == Token.DEL:  # if new_bytes is None we're guaranteed to hit this case
-                contents = b''
-                local_pos -= num
-            else:  # can only hit this case if new_bytes is not None
-                contents = new_bytes[local_pos - num:local_pos]
-            diff += f'{num}'.encode('utf-8') + Token.SEP + contents
-        writer.send(diff)
-        diff_size += len(diff)
-        if discard_diff_percentage and diff_size > discard_diff_percentage * orig_size:
+    writer = diff_file.writer(); next(writer)
+    logger.debug2('beginning diff computation')
+    for orig_bytes, new_bytes in zip_longest(orig_file.reader(), new_file.reader(), fillvalue=b''):
+        diff = bsdiff4.diff(orig_bytes, new_bytes)
+        diff_str = str(len(diff)).encode() + SEPARATOR + diff
+        total_written += len(diff_str)
+        if discard_diff_percentage and total_written > orig_size * discard_diff_percentage:
             raise DiffTooLargeException
-        pos += orig_file.block_size
+        writer.send(diff_str)
 
     return new_file.sha(), diff_file
--- tests/blob_test.py
+++ tests/blob_test.py
@@ -21,103 +21,19 @@ def test_apply_parse_error_1(mock_open_streams):
     assert 'Un-parseable diff' in str(e.value)
 
 
-def test_apply_parse_error_2(mock_open_streams):
+def test_round_trip(mock_open_streams, sha_fn):
+    new_contents = b'asdfrfsdcac'
     orig, new, diff = mock_open_streams
-    diff._fd.write(b'@3|q4|asdf')
-    with pytest.raises(DiffParseError) as e:
-        apply_diff(orig, diff, new)
-    assert 'Expected an action' in str(e.value)
+    new._fd.write(new_contents)
+    sha_fn.update(new_contents)
+    sha, _ = compute_sha_and_diff(orig, new, diff)
+    assert sha == sha_fn.hexdigest()
 
-
-def test_contents_length_error(mock_open_streams):
-    orig, new, diff = mock_open_streams
-    diff._fd.write(b'@3|I7|xy')
-    with pytest.raises(DiffParseError) as e:
-        apply_diff(orig, diff, new)
-    assert 'Un-parseable diff' in str(e.value)
-
-
-def test_apply_del(mock_open_streams):
-    orig, new, diff = mock_open_streams
-    diff._fd.write(b'@3|D2|')
+    new._fd.seek(0)
+    new._fd.write(b'')
     apply_diff(orig, diff, new)
-    assert new._fd.getvalue() == b'asdsdfa'
-
-
-def test_apply_ins(mock_open_streams):
-    orig, new, diff = mock_open_streams
-    diff._fd.write(b'@3|I2|xy')
-    apply_diff(orig, diff, new)
-    assert new._fd.getvalue() == b'asdxyfasdfa'
-
-
-def test_apply_replace(mock_open_streams):
-    orig, new, diff = mock_open_streams
-    diff._fd.write(b'@3|X2|xy')
-    apply_diff(orig, diff, new)
-    assert new._fd.getvalue() == b'asdxysdfa'
-
-
-def test_compute_sha_and_diff_eq(mock_open_streams, sha_fn):
-    orig, new, diff = mock_open_streams
-    new._fd.write(b'asdfasdfa')
     new._fd.seek(0)
-    sha_fn.update(new._fd.getvalue())
-    sha, diff = compute_sha_and_diff(orig, new, diff)
-    assert diff._fd.getvalue() == b''
-    assert sha == sha_fn.hexdigest()
-
-
-def test_compute_sha_and_diff_del(mock_open_streams, sha_fn):
-    orig, new, diff = mock_open_streams
-    orig.block_size = new.block_size = diff.block_size = 100
-    new._fd.write(b'dfasdfa')
-    new._fd.seek(0)
-    sha_fn.update(new._fd.getvalue())
-    sha, diff = compute_sha_and_diff(orig, new, diff)
-    assert diff._fd.getvalue() == b'@0|D2|'
-    assert sha == sha_fn.hexdigest()
-
-
-def test_compute_sha_and_diff_ins(mock_open_streams, sha_fn):
-    orig, new, diff = mock_open_streams
-    orig.block_size = new.block_size = diff.block_size = 100
-    new._fd.write(b'asasdfasdfa')
-    new._fd.seek(0)
-    sha_fn.update(new._fd.getvalue())
-    sha, diff = compute_sha_and_diff(orig, new, diff)
-    assert diff._fd.getvalue() == b'@2|I2|as'
-    assert sha == sha_fn.hexdigest()
-
-
-def test_compute_sha_and_diff_repl(mock_open_streams, sha_fn):
-    orig, new, diff = mock_open_streams
-    new._fd.write(b'asxyasdfa')
-    new._fd.seek(0)
-    sha_fn.update(new._fd.getvalue())
-    sha, diff = compute_sha_and_diff(orig, new, diff)
-    assert diff._fd.getvalue() == b'@2|X2|xy'
-    assert sha == sha_fn.hexdigest()
-
-
-def test_compute_sha_and_diff_orig_long(mock_open_streams, sha_fn):
-    orig, new, diff = mock_open_streams
-    new._fd.write(b'a')
-    new._fd.seek(0)
-    sha_fn.update(new._fd.getvalue())
-    sha, diff = compute_sha_and_diff(orig, new, diff)
-    assert diff._fd.getvalue() == b'@1|D1|@2|D2|@4|D2|@6|D2|@8|D1|'
-    assert sha == sha_fn.hexdigest()
-
-
-def test_compute_sha_and_diff_new_long(mock_open_streams, sha_fn):
-    orig, new, diff = mock_open_streams
-    new._fd.write(b'asdfasdfasdfa')
-    new._fd.seek(0)
-    sha_fn.update(new._fd.getvalue())
-    sha, diff = compute_sha_and_diff(orig, new, diff)
-    assert diff._fd.getvalue() == b'@9|I1|s@10|I2|df@12|I1|a'
-    assert sha == sha_fn.hexdigest()
+    assert new._fd.read() == new_contents
 
 
 def test_compute_sha_and_diff_with_large_diff(mock_open_streams):
